bd7d57819f8c55b7ab0267d89883a13c1c10cef2
alexchrysovergis <alexandroschrysovergis@gmail.com>
2024-01-23 12:33:16 -0800

    Created template files

---
+++ htdocs/fashion.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Fashion
+*/
+get_header();?>
+  
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p>You're at fashion.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer();?>
---
+++ htdocs/shop.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Shop
+*/
+get_header();?>
+  
+<section>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <p>You're at shop.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer();?>
