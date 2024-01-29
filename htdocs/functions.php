a41da75d81d172082b2cf32901ad0a871b31b692
alexchrysovergis <alexandroschrysovergis@gmail.com>
2024-01-29 12:20:45 -0800

    Linked Custom JS

--- htdocs/functions.php
+++ htdocs/functions.php
@@ -2,6 +2,7 @@
 
 function acf_files(){
   wp_enqueue_style('acf_styles', get_theme_file_uri('../src/dist/css/styles.min.css')); 
+  wp_enqueue_script('scripts', get_template_directory_uri() . '/dist/js/scripts.min.js', array('jquery'), '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'acf_files');
